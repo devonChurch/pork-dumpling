87e5d106aa2b723ecbf4807cdb091d255d8b90e0
strivegfx <notifydevon@gmail.com>
2016-01-15 21:42:19 +1300

    Commented time.js using the JSdoc syntax

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,14 +1,31 @@
+/**
+ * Timer module.
+ * @module ./timer
+ */
+
+/**
+ * Class that updates the elapsed time since starting the execution and injects
+ * it onto the  canvas.
+ */
 const Timer = class {
 
+	/**
+	 * Create a timer.
+	 * @param {class} Face - The watch face base class that this timer is bound
+	 * to.
+	 */
 	constructor(Face) {
-
-		console.log('Constructing Timer...');
 
 		this.Face = Face;
 		this.offset = this.calculateTime();
 
 	}
 
+	/**
+	 * Calculate the elapsed time by diffing the starting time vs the current
+	 * time.
+	 * @return {number} The elapsed time.
+	 */
 	calculateTime() {
 
 		const date = new Date();
@@ -19,9 +36,15 @@ const Timer = class {
 
 	}
 
+	/**
+	 * Turn the raw millisecond elapsed time into a formatted rendition that can
+	 * be injected onto the canvas.
+	 * @param {number} time - The elapsed time in milliseconds.
+	 * @return {string} The elapsed time in a 00:00 format.
+	 */
 	formatTime(time) {
 
-		// Turn into seconds.
+		/** Convert time into seconds. */
 		time = time / 1000;
 
 		const minutes = Math.floor(time / 60);
@@ -31,25 +54,40 @@ const Timer = class {
 
 	}
 
+	/**
+	 * Makes sure the current unit i.e. seconds / minutes are always comprised
+	 * of two digits.
+	 * @param {number} value - The current unit i.e. seconds / minutes.
+	 * @return {string} Current units in a two digit format.
+	 */
 	queryDigits(value) {
 
 		return `${value}`.length < 2 ? `0${value}` : `${value}`;
 
 	}
 
-	injectTime(numbers) {
+	/**
+	 * Injects the formatted elapsed time onto the canvas with a centered align
+	 * appearance, custom fonts and dynamic color.
+	 * @param {string} format - The elapsed time in a 00:00 format.
+	 */
+	injectTime(format) {
 
 		const ctx = this.Face.ctx;
 		const units = ' mins';
 		const y = 95;
-		const offset = (ctx.measureText(numbers).width + ctx.measureText(units).width) / 2;
+		/**
+		 * Centre text by offsetting half of the total combined text width from
+		 * the canvas centre.
+		 */
+		const offset = (ctx.measureText(format).width + ctx.measureText(units).width) / 2;
 		let x = (this.Face.size / 2) - offset;
 
 		ctx.font = '500 12px Roboto';
 		ctx.fillStyle = 'red';
-		ctx.fillText(numbers, x, y);
+		ctx.fillText(format, x, y);
 
-		x += ctx.measureText(numbers).width;
+		x += ctx.measureText(format).width;
 
 		ctx.font = '300 12px Roboto';
 		ctx.fillStyle = 'blue';
@@ -57,6 +95,9 @@ const Timer = class {
 
 	}
 
+	/**
+	 * Runs through the sequence in order to update the elapsed time.
+	 */
 	animate() {
 
 		const time = this.calculateTime();
@@ -68,25 +109,3 @@ const Timer = class {
 };
 
 module.exports = Timer;
-
-// var canvas  = document.getElementById("canvas");
-// var context = canvas.getContext('2d');
-// var Red = "#ff5e99";
-// var Blue = "#29a1f1";
-// var Black = "#000";
-// var x = 10;
-// var y = 40;
-// var txt_p1 = "I like ";
-// var txt_p2 = "blue";
-//
-// context.font = '14px Verdana';
-// context.fillStyle = "#000000";
-// context.fillText(txt_p1,x,y);
-// x += context.measureText(txt_p1).width;
-//
-// context.save();
-// context.font = 'normal 24px Impact';
-// context.fillStyle = Blue;
-// context.fillText(txt_p2,x,y);
-// x += context.measureText(txt_p2).width;
-// context.restore();
